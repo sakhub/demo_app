2b735441a5b0682e5490d63212aadddd5f1a4b24
sakthi <sakt006@gmail.com>
2014-01-02 16:18:07 +0530

    TDD done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,16 @@
 class PagesController < ApplicationController
+  @base_title="sdAA"
   def home
+
+  	@title="Home"
   end
 
   def contact
+  	@title="Contact"
   end
 
   def about
+  	@title="About"
   end
    
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
 
-  #Delete these examples and add some real ones
-  it "should use PagesController" do
-    controller.should be_an_instance_of(PagesController)
-  end
-
+  integrate_views 
+   before(:each) do
+   @base_title="sfagasd"
+   end
 
   describe "GET 'home'" do
     it "should be successful" do
